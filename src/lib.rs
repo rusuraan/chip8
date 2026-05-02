6962bca966653e9955bede0a7cb55f31ed598e69
rusuraan <203912170+rusuraan@users.noreply.github.com>
2026-05-02 05:20:07 +0300

    Opcode FX15

--- src/lib.rs
+++ src/lib.rs
@@ -177,6 +177,7 @@ impl Chip8 {
             (0x9, _, _, 0x0) => self.op_9xy0(x, y),
             (0xA, _, _, _) => self.op_annn(nnn),
             (0xD, _, _, _) => self.op_dxyn(x, y, n),
+            (0xF, _, 0x1, 0x5) => self.op_fx15(x),
             (0xF, _, 0x1, 0xE) => self.op_fx1e(x),
             (0xF, _, 0x2, 0x9) => self.op_fx29(x),
             (0xF, _, 0x3, 0x3) => self.op_fx33(x),
@@ -345,6 +346,11 @@ impl Chip8 {
         }
 
         self.draw_flag = true;
+        Ok(())
+    }
+
+    fn op_fx15(&mut self, x: usize) -> Result<()> {
+        self.delay_timer = self.registers[x];
         Ok(())
     }
 
